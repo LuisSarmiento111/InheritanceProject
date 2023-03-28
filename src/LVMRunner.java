ff7b6da673dc2a28bd4a0f0f81b80b7d8dd4d626
BT_4N2_30 <lsarmiento9311@bths.edu>
2023-03-28 12:30:28 -0400

    working on logic for Volume Groups

---
+++ src/LVMRunner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LVMRunner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LVM system = new LVM();
+        System.out.println("Welcome to the LVM System.");
+        String cmd = "";
+        while (!cmd.toLowerCase().equals("exit")) {
+            System.out.print("cmd#: ");
+            cmd = scanner.nextLine();
+            System.out.println(system.logic(cmd));
+        }
+    }
+}
